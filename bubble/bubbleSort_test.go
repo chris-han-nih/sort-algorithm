73afdd0698281a3ddf4c453ffd0c1f05da6c7b6f
chris.han <namilhan@gmail.com>
2022-11-13 11:51:41 +0900

    Add bubble sort test

---
+++ bubble/bubbleSort_test.go
@@ -0,0 +1,30 @@
+package bubble
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"1,2,3", args{[]int{2, 1, 3}}, []int{1, 2, 3}},
+		{"1,1,1", args{[]int{1, 1, 1}}, []int{1, 1, 1}},
+		{"empty", args{[]int{}}, []int{}},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := Sort(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("Sort() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
