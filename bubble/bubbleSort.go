214e05152cf2c2f92c6048c11c5b02d73da639e1
chris.han <namilhan@gmail.com>
2022-11-14 22:50:34 +0900

    refactoring bubble sort

--- bubble/bubbleSort.go
+++ bubble/bubbleSort.go
@@ -3,9 +3,9 @@ package bubble
 func Sort(arr []int) []int {
 	length := len(arr)
 	for i := 0; i < length; i++ {
-		for j := 0; j < length-1; j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
+		for j := i; j < length; j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
 			}
 		}
 	}
--- bubble/bubbleSort_test.go
+++ bubble/bubbleSort_test.go
@@ -16,6 +16,7 @@ func TestBubbleSort(t *testing.T) {
 	}{
 		{"1,2,3", args{[]int{2, 1, 3}}, []int{1, 2, 3}},
 		{"1,1,1", args{[]int{1, 1, 1}}, []int{1, 1, 1}},
+		{"3.5.2.4.1.7.10,6", args{[]int{3, 5, 2, 4, 1, 7, 10, 6}}, []int{1, 2, 3, 4, 5, 6, 7, 10}},
 		{"empty", args{[]int{}}, []int{}},
 	}
 	for _, tt := range tests {
